bfde4c0e1efbaf1255db21b14051efb6eb2cca97
dodilaksono <97323166+dodilaksono@users.noreply.github.com>
2022-05-31 10:35:04 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Website Api
-global.APIs = {
-	alfa : 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan le owner
-}
 
 // Free apikey
-global.APIKeys = {
-	'https://api.zeeoneofc.xyz' : 'vVhBX6IF', // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
 global.self_mode = false
